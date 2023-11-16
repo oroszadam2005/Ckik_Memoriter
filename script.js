d9c63ad7be60842b6ff4b803c1142666c8f6a8f1
Orosz Ádám <orosz.adam@ckik.loc>
2023-11-16 11:14:58 +0100

    server v2.1

---
+++ script.js
@@ -0,0 +1,82 @@
+var server_data;
+function fetchData() {
+    return fetch('http://localhost:8080/data')
+        .then(response => response.json())
+        .then(jsonData => {
+            server_data = jsonData;
+            console.log(server_data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+var data;
+function fetchAndUseData() {
+    return fetchData().then(() => {
+        data = server_data;
+    });
+}
+var kivalasztott;var szavakki = [];var rnds = [];var szavak = [];
+function Beolvasas(){   
+    fetchAndUseData().then(() => {
+        kivalasztott= data[Math.floor(Math.random() * data.length)];
+        kiszed(kivalasztott);
+    });
+} 
+function sort(arr,arr2, n)
+{
+    var i, j, temp,swap;
+    for (i = 0; i < n - 1; i++) 
+    {
+        swap = false;
+        for (j = 0; j < n - i - 1; j++) 
+        {
+            if (arr[j] > arr[j + 1]) 
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                temp = arr2[j];
+                arr2[j] = arr2[j + 1];
+                arr2[j + 1] = temp;
+                swap = true;
+            }
+        }
+        if (swap == false)break;
+    }
+}
+function kiszed(kivalasztott){
+    console.log(kivalasztott);
+    szavakki = [];rnds = [];szavak = [];
+    var db = document.getElementById("db").value;
+    document.getElementById("ebtn").disabled = false;
+    document.getElementById("db").disabled = true;
+    document.getElementById("vers").innerText = kivalasztott;
+    szavak = JSON.parse(JSON.stringify(kivalasztott.Szoveg).replace(/(?:\\[rn]|[\r\n]+)+/g,  function (x) {
+        return " "+x+" "})).split(' ');
+    for (let index = 0; index < db; index++) {
+        var rnd = Math.floor(Math.random() * szavak.length)
+        while(szavak[rnd].length <= 3 || rnds.includes(rnd) || szavak[rnd][0] == '\r'){
+            rnd = Math.floor(Math.random() * szavak.length)
+        }
+        rnds.push(rnd);
+        szavakki.push(szavak[rnd].toLowerCase().replace(',','').replace('.','').replace(':','').replace('?',''));
+    }
+    sort(rnds,szavakki,db);
+    for (let index = 0; index < db; index++) {
+        szavak[rnds[index]] = "<input type=text placeholder="+(index+1)+" id="+index+"></input>";     
+    }
+    document.getElementById("vers").innerHTML = szavak.join(' ').toString();
+}
+function ellenorzes(){
+    document.getElementById("ebtn").disabled = true;
+    document.getElementById("db").disabled = false;
+    var db = document.getElementById("db").value;var talalat = 0;
+    for (let index = 0; index < db; index++) {
+        var input = document.getElementById(index);
+        if(input.value == szavakki[index]){talalat++;input.style="  border-color: lime;";
+        }else if(szavakki.includes(input.value)){input.style="  border-color: orange;";
+        }else{input.style="  border-color: red;";}
+    }
+    document.getElementById("elabel").innerText = talalat+" / "+db;
+}
---
+++ server.js
@@ -0,0 +1,119 @@
+console.log("v2.1");
+const mysql = require('mysql');
+const express = require('express');
+const compression = require('compression');
+const pidusage = require('pidusage');
+
+const app = express();
+const port = 8080;
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "versekdb"
+});
+
+let requestCount = 0;
+let totalResponseTime = 0;
+let avgResponseTime = 0;
+let serverData = "Internal Server Error";
+
+setInterval(async () => {
+  try {
+    const start = Date.now();
+    serverData = await fetchData();
+    totalResponseTime += Date.now() - start;
+  } catch (err) {
+    console.error('Error fetching data:', err);
+    serverData = "Internal Server Error";
+  }
+}, 10000);
+
+setInterval(async () => {
+  try {
+    const [cpuUsage, scriptMemoryUsage] = await Promise.all([
+      getUsage('cpu'),
+      getUsage('memory')
+    ]);
+
+    console.log('CPU Usage:', cpuUsage.toFixed(2) + '%');
+    console.log('Script Memory Usage:', formatBytes(scriptMemoryUsage));
+    console.log('Average Response Time:', avgResponseTime.toFixed(2) + 'ms');
+
+  } catch (error) {
+    console.error('Error during monitoring:', error);
+  }
+}, 1000);
+
+app.use(compression());
+
+app.get('/data', async (req, res) => {
+  try {
+    requestCount += 1;
+    avgResponseTime = totalResponseTime / requestCount || 0;
+    res.json(serverData);
+  } catch (err) {
+    console.error('Error fetching data:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/monitor', (_, res) => {
+  const html = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Server Monitoring Dashboard</title><style>body{font-family:'Arial',sans-serif;margin:0;padding:0;background-color:#e57373;color:#ffffff;}.dashboard{display:flex;flex-direction:column;align-items:center;justify-content:center;height:100vh;}h1{color:#1976D2;}.widget{background-color:#ffffff;padding:20px;margin:20px;border-radius:8px;box-shadow:0 0 15px rgba(0,0,0,0.2);text-align:center;width:300px;}.metric-container{display:flex;flex-direction:column;align-items:center;margin-top:20px;}.metric{position:relative;text-align:center;width:80%;padding:20px;margin:15px 0;border-radius:12px;box-shadow:0 0 15px rgba(0,0,0,0.2);background-color:#1976D2;}.metric span{font-weight:bold;color:#ffffff;}#cpuUsage,#scriptMemoryUsage,#requestCount,#avgResponseTime{font-weight:bold;color:#ffffff;}}</style></head><body><div class="dashboard"><h1>Server Monitoring Dashboard</h1><div class="widget"><div class="metric-container"><div class="metric"><span>CPU Usage</span><div id="cpuUsage">Loading...</div></div><div class="metric"><span>Script Memory Usage</span><div id="scriptMemoryUsage">Loading...</div></div></div><div class="metric-container"><div class="metric"><span>Request Count</span><div id="requestCount">Loading...</div></div><div class="metric"><span>Average Response Time</span><div id="avgResponseTime">Loading...</div></div></div></div></div><script>setInterval(()=>{fetch('/monitor-data').then(response=>response.json()).then(data=>{document.getElementById('cpuUsage').innerText=data.cpuUsage;document.getElementById('scriptMemoryUsage').innerText=data.scriptMemoryUsage;document.getElementById('requestCount').innerText=data.requestCount;document.getElementById('avgResponseTime').innerText=data.avgResponseTime+"ms";}).catch(error=>console.error('Error fetching monitoring data:',error));},1000);</script></body></html>`;
+  res.send(html);
+});
+
+app.get('/monitor-data', async (_, res) => {
+  try {
+    const [cpuUsage, scriptMemoryUsage] = await Promise.all([
+      getUsage('cpu'),
+      getUsage('memory')
+    ]);
+
+    res.json({
+      cpuUsage: cpuUsage.toFixed(2) + '%',
+      scriptMemoryUsage: formatBytes(scriptMemoryUsage),
+      requestCount,
+      avgResponseTime,
+    });
+  } catch (error) {
+    console.error('Error fetching monitoring data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    pool.query("SELECT * FROM versek", (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
+
+async function getUsage(type) {
+  try {
+    const stats = await pidusage(process.pid);
+    return type === 'cpu' ? stats.cpu : stats.memory;
+  } catch (error) {
+    console.error(`Error getting ${type} usage:`, error);
+    return 0;
+  }
+}
+
+function formatBytes(bytes) {
+  const k = 1024;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  if (bytes === 0) return '0 Byte';
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return Math.round(bytes / Math.pow(k, i)) + ' ' + sizes[i];
+}
